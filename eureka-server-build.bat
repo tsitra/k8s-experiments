1b0d50fe951a808e67cb00a436a058b426f2ac26
Nathan Brown <nathan@nathanrbrown.dev>
2025-08-02 22:38:31 -0700

    smoother eureka build

--- eureka-server-build.bat
+++ eureka-server-build.bat
@@ -2,3 +2,6 @@
 docker image rm localhost:5000/eureka:local
 docker image build -f .\eureka-server.dockerfile --tag localhost:5000/eureka:local .
 docker push localhost:5000/eureka:local
+docker image save -o eureka-local.tar localhost:5000/eureka:local
+minikube image load .\eureka-local.tar
+kubectl apply -f .\eureka-server-combined.yaml
--- eureka-server-build.sh
+++ eureka-server-build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 @echo off
 docker image rm localhost:5000/eureka:local && \
-docker image build -f .\eureka.dockerfile --tag localhost:5000/eureka:local . && \
-docker push localhost:5000/eureka:local
+docker image build -f ./eureka.dockerfile --tag localhost:5000/eureka:local . && \
+docker push localhost:5000/eureka:local && \
+docker image save -o eureka-local.tar localhost:5000/eureka:local && \
+minikube image load ./eureka-local.tar
+kubectl apply -f ./eureka-server-combined.yaml
